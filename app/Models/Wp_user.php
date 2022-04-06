754886b5b208e3d0437846f4ba6733fdd6bae25c
al3xisvg <vasquezgarciaalexis@gmail.com>
2022-04-06 15:46:24 -0500

    [update] change syntax by php versions

--- app/Models/Wp_user.php
+++ app/Models/Wp_user.php
@@ -11,8 +11,17 @@ class Wp_user extends Model {
   use HasFactory;
 
   protected function fullname(): Attribute {
+    /* PHP 8
     return new Attribute(
       get: fn($value, $attributes) => strtolower($attributes['display_name']),
+    );*/
+
+    // PHP 7
+    /*return new Attribute(
+      fn($value, $attributes) => strtolower($attributes['display_name']),
+    );*/
+    return Attribute::get(
+      fn($value, $attributes) => strtolower($attributes['display_name']),
     );
   }
 
